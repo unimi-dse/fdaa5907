7ca24d60ab9c26eb2434f8ad7bb63b6bcd51c74a
tommasopessina <tom.pessina@gmail.com>
2020-01-21 14:57:32 +0100

    package required in descritpion file

--- R/uitl.R
+++ R/uitl.R
@@ -5,21 +5,6 @@ testFun <- function() {
 
 #' @export
 runFun <- function(){
-  
-  dependencies <- c("shiny", "shinyjs", "ggplot2", "scales", "Quandl", "dplyr",
-                    "readr", "forecast", "zoo", "rvest","xml2","stringr")
-  
-  for (x in dependencies) {
-    if(x %in% rownames(installed.packages()) == T) {
-      library(x, character.only = T)
-    } else {
-      install.packages(x)
-      library(x, character.only = T)
-    }
-  }
-  
-  print("Packages installed")
-  
  
   # run app -----------------------------------------------------------------
   runApp( shinyApp(ui, server), launch.browser = T )
