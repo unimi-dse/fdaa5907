13e4d2be5e2d1c4d374bce420dd006769853be03
tommasopessina <tom.pessina@gmail.com>
2020-01-21 16:03:25 +0100

    fix

--- R/uitl.R
+++ R/uitl.R
@@ -11,7 +11,8 @@ installPack <- function()
   packages  =c("shiny", "shinyjs", "ggplot2", "scales", "Quandl", "dplyr", "readr", "forecast", "zoo", "rvest", "xml2", "stringr")
   if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
     install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")
-  }
+  }else
+    print("everything up to date")
 }
 
 #' @export
