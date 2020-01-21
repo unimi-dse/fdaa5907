b3abf9f419c00004d5d015945b2586c3f50fbc3e
tommasopessina <tom.pessina@gmail.com>
2020-01-21 16:38:47 +0100

    change

---
+++ R/test.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(scales)
+library(Quandl)
+library(dplyr)
+library(readr)
+library(forecast)
+library(zoo)
+
+eth <- Quandl("BITFINEX/ETHUSD", api_key="-GNJxjPntak8s-AxpM5o")
+
+ethts <- Quandl("BITFINEX/ETHUSD", api_key="-GNJxjPntak8s-AxpM5o", type = "xts")
+
+ggplot(eth, aes(x = eth$Date, y = eth$Last)) +
+  labs(x = "Date", y = "Price") +
+  geom_line() +
+  scale_x_date(labels = date_format("%Y-%m-%d"))
+
+#sm <- ma(eth$Last,order=12)
+#lines(eth$date,sm,col="green")
+
+startDF <- data.frame("Date"=eth$Date, "Price"=eth$Last)
+
+smDF <- data.frame("Date"=eth$Date,"MA"=rollmean(startDF$Price, k = 13, fill = NA))
+
+#arDF <- data.frame("Date"=eth$Date,"AR"=arima.sim(list(ar=c(0.5)),n=100,innov=eth$Price))
+
+# Automated forecasting using an exponential model
+fit <- ets(startDF$Price)
+plot(fit)
+#df1 <- data.frame("Date"=eth$Date,"fit"=fit)
+
+# Automated forecasting using an ARIMA model00
+fit2 <- auto.arima(startDF$Price)
+plot(fit2)
+#df2 <- data.frame("Date"=eth$Date,"fit2"=fit2)
+
+ggplot() + 
+  geom_line(aes(x=Date, y=Price, colour="Ethereum price"), startDF) +  
+  geom_line(aes(x=Date, y=MA, colour="Moving average"), smDF) 
--- R/uitl.R
+++ R/uitl.R
@@ -1,24 +1,22 @@
-library(shiny)
 
 #' @export
 testFun <- function() {
   print("Yes, it works!")
 }
 
-#installPack <- function()
-#{
-#  packages  =c("shiny", "shinyjs", "ggplot2", "scales", "Quandl", "dplyr", "readr", "forecast", "zoo", "rvest", "xml2", "stringr","tidyverse")
-#  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
- #   install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")
-#  }else
-#    print("everything up to date")
-#  for(x in c)
-#    library(x)
-#}
+#' @export
+installPack <- function()
+{
+  packages  =c("shiny", "shinyjs", "ggplot2", "scales", "Quandl", "dplyr", "readr", "forecast", "zoo", "rvest", "xml2", "stringr")
+  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")
+  }else
+    print("everything up to date")
+}
 
 #' @export
 runFun <- function(){
- 
+  library(shiny)
   # run app -----------------------------------------------------------------
   runApp( shinyApp(ui, server), launch.browser = T )
 }
