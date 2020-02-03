af1de532b2052d45f81c5f2186825e7491c306d4
Tommaso Pessina <20018259@studenti.uniupo.it>
2020-02-03 15:43:28 +0100

    delete exported function

--- inst/shiny/GUI/server.R
+++ inst/shiny/GUI/server.R
@@ -6,6 +6,20 @@ setClass(Class="pricePerc",
          )
 )
 
+#' Set the input value in the correct output widget of the UI
+#' @param price Actual price of Ehtereum index
+#' @param perc Actual percentage of change in the Ethereum price index
+#' @param output The output widget of the UI
+#' @examples
+#' setOutPricePerc("$164,37", "(1,23)", output)
+setOutPricePerc <- function(price,perc, output){
+  output$price <- shiny::renderText(price)
+  output$percent <- shiny::renderText(perc)
+  tm <- Sys.time()
+  ts <- format(tm, format = "%Y-%m-%d %H:%M:%S", tz = "", usetz = FALSE)
+  output$hour <- renderText(ts)
+}
+
 #' Define the server logic
 #' #'
 server <- function(input, output, session) {
