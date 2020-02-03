b395d5a9e07587054f610da11d8adf880a2d7157
tommasopessina <tom.pessina@gmail.com>
2020-02-03 13:56:00 +0000

    try without api key

--- R/getEth.R
+++ R/getEth.R
@@ -3,5 +3,7 @@
 #' @return The data of all Ehtereum index
 #' @export
 getEth <- function(){
-  return(eth <- Quandl::Quandl("BITFINEX/ETHUSD", api_key="-GNJxjPntak8s-AxpM5o"))
+  return(eth <- Quandl::Quandl("BITFINEX/ETHUSD"))
 }
+
+#, api_key="-GNJxjPntak8s-AxpM5o"
