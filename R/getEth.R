0d22d0832c20fc078f9cc6cfb463409bacdce7a0
tommasopessina <tom.pessina@gmail.com>
2020-02-03 13:59:54 +0000

    revert last change

--- R/getEth.R
+++ R/getEth.R
@@ -3,7 +3,5 @@
 #' @return The data of all Ehtereum index
 #' @export
 getEth <- function(){
-  return(eth <- Quandl::Quandl("BITFINEX/ETHUSD"))
+  return(eth <- Quandl::Quandl("BITFINEX/ETHUSD", api_key="-GNJxjPntak8s-AxpM5o"))
 }
-
-#, api_key="-GNJxjPntak8s-AxpM5o"
