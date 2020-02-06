4e5b3810345627913a65d39ecc3cb3e608eb3a07
tommasopessina <tom.pessina@gmail.com>
2020-02-06 14:06:21 +0000

    test

--- R/getEth.R
+++ R/getEth.R
@@ -4,11 +4,27 @@
 #' @export
 getEth <- function(){
   key = ethereum.key
+  print(key)
   if(is.null(key)){
     stop("No api_key provided")
   }else{
     return(eth <- Quandl::Quandl("BITFINEX/ETHUSD", api_key=key))
   }
 }
+
+#' Set API key for getting the Ether data
+#'
+#' @return TRUE if the key is setted else FALSE
+#' @example setKeyAPI("abc123")
+#' @export
+setKeyAPI <- function(apikey){
+  ethereum.key = apikey
+  if(is.null(ethereum.key))
+   return(FALSE)
+  else
+   return(TRUE)
+}
+
+
 
 # "-GNJxjPntak8s-AxpM5o"
